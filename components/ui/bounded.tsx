25384fd86764caa9db20f3e5080bbf5809d6f64c
Dumitru-radu <101594913+radu9120@users.noreply.github.com>
2025-05-23 13:53:30 +0100

    Add Bounded component for layout consistency and improved structure in Hero

---
+++ components/ui/bounded.tsx
@@ -0,0 +1,23 @@
+"use client";
+import clsx from "clsx";
+
+type BoundedProps = {
+  as?: React.ElementType;
+  className?: string;
+  children: React.ReactNode;
+};
+
+export default function Bounded({
+  as: Comp = "section",
+  className,
+  children,
+  ...restProps
+}: BoundedProps) {
+  return (
+    <Comp className={clsx("lg:py-24 xl:py-24 py-10", className)} {...restProps}>
+      <div className="mx-auto px-4 md:px-6 lg:px-12 w-full max-w-[1500px] space-y-4 ">
+        {children}
+      </div>
+    </Comp>
+  );
+}
